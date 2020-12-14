f85da7fb55e645c347bbf8e7c1b6f92756f6cf5e
PavelVasilev <vpnprint@gmail.com>
2020-12-14 23:56:03 +0300

    feat: solved some tasks

--- src/08-conditions-n-loops-tasks.js
+++ src/08-conditions-n-loops-tasks.js
@@ -243,8 +243,9 @@ function reverseString(/* str */) {
  *   87354 => 45378
  *   34143 => 34143
  */
-function reverseInteger(/* num */) {
-  throw new Error('Not implemented');
+function reverseInteger(num) {
+  return num.toString().split('').reverse().join('');
+  // throw new Error('Not implemented');
 }
 
 /**
